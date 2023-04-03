e4dd654ff185b12b9cbf87fecb11c309b6348e2c
Maria Luisa Gomes Luz <14753135608@DC01.IFS.IFSULDEMINAS.EDU.BR>
2023-04-03 08:41:21 -0300

    Aula 16

--- aula16/index.php
+++ aula16/index.php
@@ -1,3 +1,9 @@
+<?php
+  include_once "dados.php";
+
+
+?>
+
 <!doctype html>
 <html lang="en">
   <head>
@@ -42,32 +48,30 @@
 
     <nav class="navbar navbar-expand-lg bg-body-tertiary">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">Navbar</a>
+    <a class="navbar-brand" href="#">Menu</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Link</a>
-        </li>
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Dropdown
+  
+       <?php 
+       $categorias =[];
+       foreach($produtos as $key =>$value): 
+        if (!in_array($value["categoria"], $categorias)):
+          $categorias[] = $value ["categoria"];
+          ?>
+      <li class="nav-item">
+          <a class="nav-link" href="#">
+            <?php echo $value["categoria"];?>
           </a>
-          <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="#">Action</a></li>
-            <li><a class="dropdown-item" href="#">Another action</a></li>
-            <li><hr class="dropdown-divider"></li>
-            <li><a class="dropdown-item" href="#">Something else here</a></li>
-          </ul>
         </li>
-        <li class="nav-item">
-          <a class="nav-link disabled">Disabled</a>
-        </li>
+
+        <?php
+        endif;
+        endforeach;
+        ?>
+      
       </ul>
       <form class="d-flex" role="search">
         <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
@@ -80,25 +84,32 @@
     <!--final menu -->
 
     <!--Lista de produtos-->
-    <div class="container">
+   <hr>
+   <div class="container">
 
-    <div class="row row-cols-4">
-    <div class="card" style="width: 18rem;">
-    <img src="..." class="card-img-top" alt="...">
+    <div class="row row-cols-4 g-4">
+
+    <?php
+
+    foreach($produtos as $key => $produtos): 
+    ?>
+    <div class="col">
+    <div class="card" >
+    <img src="<?php echo $produtos["foto"]; ?>" class="card-img-top" alt="...">
     <div class="card-body">
-      <h5 class="card-title">Card title</h5>
-      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-      <a href="#" class="btn btn-primary">Go somewhere</a>
+      <h5 class="card-title"><?php echo $produtos["nome"]; ?></h5>
+      <a href="#" class="btn btn-primary">Ver produto</a>
     </div>
     </div>
+    </div>
+    <?php endforeach; ?>
  </div>
 </div>
 
     <!--Final da Lista de produtos-->
 
 
-    <h1>Hello, world!</h1>
+   <hr>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/js/bootstrap.bundle.min.js" integrity="sha384-qKXV1j0HvMUeCBQ+QVp7JcfGl760yU08IQ+GpUo5hlbpg51QRiuqHAJz8+BrxE/N" crossorigin="anonymous"></script>
   </body>
 </html>
-
