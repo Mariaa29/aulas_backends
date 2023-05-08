55492b1174a559d6407d3b0da3deba6e57b6f4af
Maria Luisa Gomes Luz <14753135608@DC01.IFS.IFSULDEMINAS.EDU.BR>
2023-05-08 08:41:07 -0300

    Aula 17 correção insert

---
+++ aula17/produto/excluir.php
@@ -0,0 +1,30 @@
+<?php
+
+require_once "../conexao.php";
+
+//verefica se foi enciado o parametro id
+if(isset($_GET['id'])){
+
+
+//pega o valor com o id que foi enviado pela URL
+$id = $_GET['id'];
+
+
+//String com o comando SQL para ser executado no DB 
+$sql = "DELETE FROM `produto` WHERE  `idproduto`= ?;"; 
+
+
+
+//Prepara o SQL para ser executado no banco de dados
+$comando= $conexao->prepare($sql);
+
+//Adiciona os valores nos parâmetros
+$comando->bind_param("i", $id);
+
+//executa o SQL - Comando no Banco de Dados 
+$comando->execute();
+
+}
+
+//abre o arquivo form.php, vai para um arquivo e ja volta para o outro
+header("Location: form.php");
